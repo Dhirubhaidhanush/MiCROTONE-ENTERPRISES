88c509ef98ba403fccfc1942821a1d755e45e5bb
Dhirubhai <146410587+Dhirubhaidhanush@users.noreply.github.com>
2026-02-09 12:11:48 +0530

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -61,7 +61,7 @@ export default function Contact() {
             </div>
             <div className="flex items-center gap-3">
               <Mail className="w-5 h-5 text-blue-400" />
-              <span>dhirubhaisairaj@gmail.com</span>
+              <span>microtoneofficial22@gmail.com</span>
             </div>
           </div>
         </div>
